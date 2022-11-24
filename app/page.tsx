bc8e995473c89459aa6e5a9c28f0f4afa1d3dc67
nanchezal <mapodaegyo12@gmail.com>
2022-11-24 16:39:38 +0900

    create the random users section of the widgets component

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,34 @@ import FeedLayout from "./feed/feedlayout";
 import Widgets from "./widget/widgets";
 import { ReactElement } from "react";
 
-async function getData() {
+const getNewsData = async () => {
   const res = await fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/us.json");
 
   if(!res.ok) {
     throw new Error('Failed to fetch data');
   }
 
+
+
   return res.json();
 }
 
+const getUsersData = async () => {
+    // who to follow section
+    const res = await fetch("https://randomuser.me/api/?results=30&inc=name,login,picture");
+
+    if(!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    return res.json()
+}
+
+
 
 export default async function Page() {
-   const newsResults = await getData();
+   const newsResults = await getNewsData();
+   const randomUsersData = await getUsersData();
   return (
     <div>
       <main className="flex min-h-screen mx-auto">
@@ -26,7 +41,7 @@ export default async function Page() {
         {/* feed */}
         <FeedLayout />
         {/* widget */}
-        <Widgets newsResults={newsResults}/>
+        <Widgets newsResults={newsResults} randomUsersData={randomUsersData.results}/>
         {/* modal */}
 
       </main>
